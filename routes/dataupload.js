f9bea6c72c3b2f2822389729f1279f6c81d4f876
Lime029 <46301337+Lime029@users.noreply.github.com>
2022-11-22 12:06:51 -0500

    Revert "Fixed data entry bug, data input increment, error handling"

--- routes/dataupload.js
+++ routes/dataupload.js
@@ -24,7 +24,6 @@ const getBlobName = originalName => {
     return `${identifier}/${originalName}.txt`;
 };
 
-// POST dataupload page
 router.post('/', uploadStrategy, (req, res) => {
     const blobService = [];
     
@@ -34,12 +33,11 @@ router.post('/', uploadStrategy, (req, res) => {
     }
     
     for(let i = 1; i <= 6; i++){
-        if(req.body[('value' + i)]){
+        if(req.body[('value' + i)] != ''){
             let value = req.body[('value' + i)] + ' ';
-            let date = req.body[('date' + i)] + '\n';
-            console.log(req.body[('value' + i)]);
-            blobService[i - 1].appendBlock(value, value.length).catch((err)=>{if(err) {handleError(err,res);return;}});
-            blobService[i - 1].appendBlock(date, date.length).catch((err)=>{if(err) {handleError(err,res);return;}});
+            let date = req.body[('date' + i)] + ' ';
+            blobService[i - 1].appendBlock(value, value.length).catch((err)=>{if(err) {handleError(err);return;}});
+            blobService[i - 1].appendBlock(date, date.length).catch((err)=>{if(err) {handleError(err);return;}});
         }
     }
     res.render('success', { 
